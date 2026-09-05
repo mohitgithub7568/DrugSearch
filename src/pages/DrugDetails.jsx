bff901297c4d594a7f328c24a3c7c94d6871475d
Mohit Soni <creature7568@gmail.com>
2026-09-05 10:41:33 +0530

    Added routes

---
+++ src/pages/DrugDetails.jsx
@@ -0,0 +1,5 @@
+function DrugDetails() {
+  return <h1>Drug details</h1>;
+}
+
+export default DrugDetails;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Drug Search</h1>;
+}
+
+export default Home;
